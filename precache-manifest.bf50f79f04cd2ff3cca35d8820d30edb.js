f39dfc18a594322a5827a65b720edbe3d2561491
Cathy <cathy.xyy@gmail.com>
2020-08-10 14:25:50 +0800

    Updates

---
+++ precache-manifest.bf50f79f04cd2ff3cca35d8820d30edb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16ca0488325b74c50d4f66d038cb715",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b40a383ca46a6ab5ff21",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e876d5e829cfc21fd9f0",
+    "url": "/test/static/js/2.9ae699c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.9ae699c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40a383ca46a6ab5ff21",
+    "url": "/test/static/js/main.c540ab34.chunk.js"
+  },
+  {
+    "revision": "a2a118daa8ca02cff825",
+    "url": "/test/static/js/runtime-main.5ebfacb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
